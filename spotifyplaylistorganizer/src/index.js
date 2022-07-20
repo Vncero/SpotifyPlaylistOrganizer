a00d5ee1d52bcf52156a04022e07380934978b4f
Vncero <vincentzheng1423@gmail.com>
2022-07-20 23:44:39 +0000

    setting up

--- spotifyplaylistorganizer/src/index.js
+++ spotifyplaylistorganizer/src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './site.css';
 
-class Button extends React.Component {
+class Login extends React.Component {
     render() {
         return (
-            <Button>{"Wow"}</Button>
+            <Form>
+                
+            </Form>
         );
     }
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Button />);
+root.render(<Login />);
--- spotifyplaylistorganizer/src/organizer.js
+++ spotifyplaylistorganizer/src/organizer.js
@@ -1,7 +1,42 @@
-import {Client, TrackHandler, PlaylistHandler} from 'spotify-sdk';
+import {Client, UserHandler, TrackHandler, PlaylistHandler} from 'spotify-sdk';
 
 const client = Client.instance;
 
 client.settings = {
-    
+    clientId: '',
+    secretId: '',
+    scopes: [
+        'playlist-read-collaborative',
+        'playlist-modify-public',
+        'playlist-read-private',
+        'playlist-modify-private',
+    ],
+    redirect_uri: '',
+};
+
+function session() {
+    if (sessionStorage.token) {
+        client.token = sessionStorage.token;
+    } else if (window.location.hash.split('&')[0].split('=')[1]) {
+        sessionStorage.token = window.location.hash.split('&')[0].split('=')[1];
+        client.token = sessionStorage.token;
+    }
+}
+
+session();
+
+function login() {
+    client.login().then((url) => {
+        window.location.href = url;
+    });
+}
+
+document.querySelector('#login').onclick = login;
+
+let user = new UserHandler();
+let playlists = new PlaylistHandler();
+let tracks = new TrackHandler();
+
+function sortBySound(playlist) {
+    playlists.
 }
